3b76957a419a2f94ea6f84dacd62b26d59dda313
Angelo Henrique Ormelezi Andrade <115271619+Ormelezi@users.noreply.github.com>
2024-02-22 10:44:47 -0300

    Add files via upload

---
+++ DW2S5-main/praticando02.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documento</title>
+</head>
+    <body>
+        <h1><? echo "Essa é minha segunda página PHP";?></h1>
+        <h2> <? echo "Essa é uma outra página." ?></h2>
+        <a href="hello.php"> <? echo "Voltar" ?></a>
+    </body>
+</html>
