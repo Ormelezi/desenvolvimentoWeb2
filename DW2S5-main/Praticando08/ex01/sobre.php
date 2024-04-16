9585a7df521714e62853a817e7b2942013eb3b50
Angelo Henrique Ormelezi Andrade <115271619+Ormelezi@users.noreply.github.com>
2024-04-16 09:11:35 -0300

    Add files via upload

---
+++ DW2S5-main/Praticando08/ex01/sobre.php
@@ -0,0 +1,17 @@
+<?php require_once 'cabecalho.php'; ?>
+
+<div class="inicio">
+    <div class="bg-light p-4 mb-4 rounded">
+        <h1 class="text-center">Sobre</h1>
+    </div>
+
+    <img class="img-lorem img-thumbnail m-4 rounded float-end" src="https://live.staticflickr.com/516/31415429983_bf858be445_b.jpg" alt="Lorem Ipsum">
+    <h3>
+        São Paulo é a maior cidade da América Latina
+    </h3>
+    <p>   
+        Além de ser a cidade mais populosa do Brasil, São Paulo também está no pódio entre os municípios da América Latina no quesito número de habitantes. A capital paulista é seguida pela Cidade do México, que tem cerca de 9,2 milhões de habitantes segundo o censo do país. 
+    </p>
+</div>
+
+<?php require_once 'rodape.php'; ?>
